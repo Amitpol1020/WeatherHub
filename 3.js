f925ff11fceb167cb9dbec51eb315b7bd534a812
Amitpol1020 <144685471+Amitpol1020@users.noreply.github.com>
2024-06-02 22:31:52 +0530

    Update 3.js

--- 3.js
+++ 3.js
@@ -1,24 +1,25 @@
 function getWeather() {
-    const locationInput = document.getElementById('locationInput');
-    const weatherInfo = document.getElementById('weatherInfo');
+    const location = document.getElementById('location');
+    const weather = document.getElementById('weather');
 
-    const apiKey = '902f311f7959e84e66c5f6a5aeac4ada';
+    const Key = '902f311f7959e84e66c5f6a5aeac4ada';
 
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${locationInput.value}&appid=${apiKey}`;
+    const Url = `https://api.openweathermap.org/data/2.5/weather?q=${location.value}&appid=${Key}`;
 
-    fetch(apiUrl)
+    fetch(Url)
         .then(response => response.json())
         .then(data => {
             const temperature = Math.round(data.main.temp - 273.15);
             const description = data.weather[0].description;
-            const humidity = data.main.humidity;
+			const humidity = data.main.humidity;
             
-            weatherInfo.innerHTML = `<p><strong>Temperature: ${temperature}°C</strong></p>
+            weather.innerHTML = `<p><strong>Temperature: ${temperature}°C</strong></p>
                                      <p><strong>Description: ${description}</strong></p>
-                                     <p><strong>humidity: ${humudity}%</strong></p>``;
+									 <p><strong>Humidity : ${humidity}%</strong></p>`;
         })
         .catch(error => {
             console.error('Error fetching weather data:', error);
-            weatherInfo.innerHTML = '<p>Failed to fetch weather data. Please try again.</p>';
+            weather.innerHTML = '<p>Failed to fetch weather data. Please try again.</p>';
         });
 }
+
